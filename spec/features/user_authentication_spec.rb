46f0c38144d2b1c369414b75fb1f6606b0ce6ce5
Marianne Rachid <marianne.rachid@gmail.com>
2018-11-03 14:13:44 +0000

    fix test

--- spec/features/user_authentication_spec.rb
+++ spec/features/user_authentication_spec.rb
@@ -9,4 +9,17 @@ RSpec.feature 'User', type: :feature do
     sign_up
     expect(page).to have_content("Welcome! You have signed up successfully.")
   end
+
+  scenario 'Can log in' do
+    sign_up
+    click_on 'Logout'
+    log_in
+    expect(page).to have_content('Signed in successfully.')
+  end
+
+  scenario 'Can Log out' do
+   sign_up
+   click_on 'Logout'
+   expect(page).to have_content('Signed out successfully.')
+ end
 end
